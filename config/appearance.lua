4599f7a665cabe35772191c4b9b619b6223ac187
thekingofcool <offweiboforawhile@proton.me>
2024-11-03 09:29:46 +0800

    adjust default window size

--- config/appearance.lua
+++ config/appearance.lua
@@ -40,6 +40,8 @@ return {
    switch_to_last_active_tab_when_closing_tab = true,
 
    -- window
+   initial_cols = 95,
+   initial_rows = 60,
    window_padding = {
       left = 0,
       right = 0,
